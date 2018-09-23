4b24c9f8a4b49d11d9ab2db86468c10944005183
Wei Yuheng <651633251@qq.com>
2018-09-23 14:50:21 +0800

    modified doom_rl.utils.core.model and made it easier to be extended

--- doom_rl/utils/core/model.py
+++ doom_rl/utils/core/model.py
@@ -81,7 +81,7 @@ class DQNTfModel(Model):
     """
     This is a half-implemented DQN model based on pure tensorflow. All abstract methods defined
     in base class `Model` are implemented.
-    You can easily extend this class by providing a `_build_network()` method in your subclass.
+    You can easily extend this class by just implementing `_build_network()` method in your subclass.
 
     # Arguments:
         * `state_shape` The shape of input states.
@@ -96,6 +96,9 @@ class DQNTfModel(Model):
         self.nb_actions = nb_actions
         self.lr = learning_rate
 
+        # Tensorflow session
+        self._session = None
+
         # State input of the network
         self.s_input = tf.placeholder(tf.float32, shape=[None] + list(self.state_shape), name='State')
 
@@ -108,17 +111,22 @@ class DQNTfModel(Model):
         # Output q values, must be used in _build_network() method as the output of the network
         self.q_values = None
 
+        # Optimizer,
+        self._optimizer = None
+
+        # Build the network
+        self._build_network(self.lr)
+
+        # Check whether the definition for self.q_values and self._optimizer are provided
+        assert self.q_values is not None, 'The output of the network is not defined.'
+        assert self._optimizer is not None, 'No optimizer is provided.'
+
         # Operation nodes defined by _build_network() method
-        self._max_q_values = None
-        self._best_action = None
-        self._action_q_values = None
-        self._loss = None
-        self._session = None
-
-        # Training operation
-        self._train = None
-
-        self._build_network()
+        self._max_q_values = tf.reduce_max(self.q_values, axis=1)
+        self._best_action = tf.argmax(self.q_values, axis=1)
+        self._action_q_values = tf.reduce_sum(self.q_values * tf.one_hot(self.a_input, self.nb_actions), axis=1)
+        self._loss = tf.losses.mean_squared_error(self._action_q_values, self.target_q_values)
+        self._train = self._optimizer.minimize(self._loss)
 
     def save_weights(self, save_path):
         tf.train.Saver().save(self.session, save_path)
@@ -127,43 +135,34 @@ class DQNTfModel(Model):
         tf.train.Saver().restore(self.session, load_path)
 
     def train(self, state, action, target_q):
-        assert self._train is not None, 'Training operation can not be None.'
-
         state = self.preprocess_state_batch(state)
         l, _ = self.session.run([self._loss, self._train],
                                 {self.s_input: state, self.target_q_values: target_q, self.a_input: action})
         return l
 
     def get_best_action(self, state):
-        assert self.q_values is not None, 'The output of the network can not be None.'
-
         state = self.preprocess_state_batch(state)
         return self.session.run(self._best_action, {self.s_input: state})[0]
 
     def get_q_values(self, state):
-        assert self.q_values is not None, 'The output of the network can not be None.'
-
         state = self.preprocess_state_batch(state)
         return self.session.run(self.q_values, {self.s_input: state})
 
     def get_max_q_values(self, state):
-        assert self.q_values is not None, 'The output of the network can not be None.'
-
         state = self.preprocess_state_batch(state)
         return self.session.run(self._max_q_values, {self.s_input: state})
 
-    def _build_network(self):
+    def _build_network(self, learning_rate):
         """
-        Build the model's network.
+        Build the model's network. (Example implementation in doom_rl.utils.models.SimpleTfModel)
+
         To implement this method you should:
         1. Build the structure of your network. Use self.s_input as your network's input and self.q_values
            as your network's output.
-        2. The loss of the network is already defined as self._loss, however, you need to provide an
-           optimizer and the definition of self._train for training operation.
-        3. self._max_q_values = tf.reduce_max(self.q_values, axis=1)
-           self._best_action = tf.argmax(self.q_values, axis=1)
-           self._action_q_values = tf.reduce_sum(self.q_values * tf.one_hot(self.a_input, self.nb_actions), axis=1)
-           self._loss = tf.losses.mean_squared_error(self._action_q_values, self.target_q_values)
+        2. The loss of the network is already defined as self._loss and the training operation defined as
+           self._train = self._optimizer.minimize(self._loss), however, you need to provide the definition
+           for self._optimizer in this method. (Adam optimizer, RMSProp optimizer etc.)
+        3. Argument `learning_rate` can be used for constructing self._optimizer.
         """
         pass
 
--- doom_rl/utils/models.py
+++ doom_rl/utils/models.py
@@ -11,7 +11,7 @@ class SimpleTfModel(DQNTfModel):
         super(SimpleTfModel, self).__init__(state_shape, nb_actions,
                                             learning_rate, preprocess_state_batch=preprocess_state_batch)
 
-    def _build_network(self):
+    def _build_network(self, learning_rate):
         conv1 = conv2d(self.s_input, 24, 6, strides=(3, 3), activation=relu,
                        kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                        bias_initializer=tf.constant_initializer(0.01), name='ConvLayer1')
@@ -27,9 +27,4 @@ class SimpleTfModel(DQNTfModel):
                               kernel_initializer=tf.contrib.layers.xavier_initializer(),
                               bias_initializer=tf.constant_initializer(0.01))
 
-        self._max_q_values = tf.reduce_max(self.q_values, axis=1)
-        self._best_action = tf.argmax(self.q_values, axis=1)
-        self._action_q_values = tf.reduce_sum(self.q_values * tf.one_hot(self.a_input, self.nb_actions), axis=1)
-        self._loss = tf.losses.mean_squared_error(self._action_q_values, self.target_q_values)
-        opt = Adam(self.lr)
-        self._train = opt.minimize(self._loss)
+        self._optimizer = Adam(learning_rate)
